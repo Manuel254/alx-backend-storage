7af3e31b4b3506548de29f22bf067d7ca30c7c6f
Manuel254 <immanuelkituku@gmail.com>
2023-07-12 16:05:44 +0000

    Fix folder path

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- Creates a table users
+-- A user table with id, email and name as attributes
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY(id)
+);
+

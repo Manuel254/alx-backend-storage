74181e32843fd0d5436e8e514d3cc8bf25b515d3
Manuel254 <immanuelkituku@gmail.com>
2023-07-13 06:21:02 +0000

    Fix script

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
 -- create an index for optimization
---CREATE INDEX IF NOT EXISTS origin_fan_count ON metal_bands (origin, fans);
+CREATE INDEX origin_fan_count ON metal_bands (origin, fans);
 
 -- select only the origin column and fans column an order fans
 SELECT origin, SUM(fans) as nb_fans

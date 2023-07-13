bda2a70e692c9229aa6d0e250a1b42d5e3417d3f
Manuel254 <immanuelkituku@gmail.com>
2023-07-13 06:25:15 +0000

    Fix script

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,10 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
+-- check if index exists
+SELECT COUNT(*)
+FROM INFORMATION_SCHEMA.STATISTICS
+WHERE TABLE_SCHEMA 'holberton'
+  AND TABLE_NAME = 'metal_bands'
+  AND INDEX_NAME = 'origin_fan_count';
 -- create an index for optimization
 CREATE INDEX origin_fan_count ON metal_bands (origin, fans);
 

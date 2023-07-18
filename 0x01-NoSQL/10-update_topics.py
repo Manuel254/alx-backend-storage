ed5056bd6a9b9c89599b9e347f8e6c58332cc1fa
Manuel254 <imanuelkituku@gmail.com>
2023-07-17 22:49:23 -0700

    Update a document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Changes all topics of a school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates topics in a document"""
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})

9537e7ba1d1944b35f91a7e013afd647b0b60d3b
Manuel254 <immanuelkituku@gmail.com>
2023-07-19 15:36:31 +0000

    Increment values when methods are called

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,24 @@ and stores the input data it gets from the parameter as the value
 import redis
 import uuid
 from typing import Union, Optional, Callable, Any
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator function that counts how many times
+    methods of the Cache class are called
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function that performs the increment
+        functionality
+        """
+        val = method(self, *args, **kwargs)
+        self._redis.incr(key)
+        return val
+    return wrapper
 
 
 class Cache:
@@ -16,20 +34,22 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores key-value pair in db"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Union[Callable[..., Any], None]) -> Any:
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """Gets a value of a key and converts it
         to its desirable format
         """
         result = self._redis.get(key)
 
-        if result is not None and fn is not None:
-            return fn(result)
+        if fn:
+            result = fn(result)
         return result
 
     def get_str(self, key: str) -> str:
